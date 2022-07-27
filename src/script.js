3e45b46b2b7ef086a082fcafcf1034433c28ab8f
swetlaia <briefsw@gmail.com>
2022-07-27 19:44:09 +0200

    API for Forecast

--- src/script.js
+++ src/script.js
@@ -26,7 +26,8 @@ let realTime = new Date();
 
 h2.innerHTML = formatDate(realTime);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -46,6 +47,12 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "6d8a5d8283fcd030e4e083d33b953304";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&appid=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function currentWeather(response) {
